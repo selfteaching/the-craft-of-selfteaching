f9619f719070f355cf71f35668d427487cd9fa93
PsChina <panshanzxcv.love@163.com>
2019-04-09 21:15:47 +0800

    enterys 的阅读

---
+++ my-notes/my-notes.py
@@ -0,0 +1,48 @@
+# coding=UTF-8
+# import random
+# r = random.randrange(1, 1000)
+# # 布尔运算
+# print( 1 == 2 )
+# print( 1 != 2 )
+
+# # 流程控制
+# if(r%2 == 0):
+#     print(r, 'is even.')
+# else:
+#     print(r, 'is odd.')
+
+# for i in range(10):
+#     if i%2 ==1:
+#         print(i)
+
+# print('\n')
+
+# for n in range(2, 100):
+#     for i in range(2, n):
+#         if n%i == 0:
+#             break
+#     else:
+#         print(n)
+
+# # 所谓算法
+# for n in range(2,100):
+#     for i in range(2, int(n**0.5)+1):
+#         if n%i == 0:
+#             break
+#     else:
+#         print(n)
+# # 所谓函数
+# a = abs(-3.1415926)
+# print(a)
+# def is_prime(n):
+#     for i in range(2, int(n**0.5)+1):
+#         if n%i == 0:
+#             return False
+#     else:
+#         return True
+
+# for i in range(80,100):
+#     if is_prime(i):
+#         print(i)
+# # 细节补充
+
