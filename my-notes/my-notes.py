9c8670f4d9370d196519f5a6d171dc8afe71fb7f
chaxiaoli001 <50093760+chaxiaoli001@users.noreply.github.com>
2019-10-17 15:02:53 +0800

    Part.1.E.1.entrance note-chaxiaoli
    Part.1.E.1.entrance note-chaxiaoli

---
+++ my-notes/my-notes.py
@@ -0,0 +1,32 @@
+逻辑操作符（Logical Operators）
+		比较操作符:== != > >= < <= in
+		运算对象（Operands）:数字值和字符串值
+		返回值：布尔值		
+
+布尔运算操作符:
+	与and 或or 非not
+	布尔值：True False
+	
+流程控制（Control Flow）:
+	分支：if/else 语句
+	r=2 #将整数常量（Literal）2赋值（Assignment）给变量（Variable）r
+	for循环（loop）
+
+函数（Functions）
+	函数名（Function Name）、参数（Parameters）、返回值（Return Value）、调用（Call）
+	程序：核心构成部分就是输入、处理、输出。
+	子程序（Sub-Program）：主程序执行到函数调用时，就开始执行实现函数的那些代码，而后再返回主程序
+	
+其他：
+	语句：
+		一个完整的程序，由1个或多个语句（statements）构成
+	语句块：
+		行首空白（Leading whitespace）：由空格 `' '` 或者 Tab `⇥` 构成
+		Python将认为这一行与其他邻近有着相同行首空白的语句同属于一个**语句块**;
+		而一个语句块必然由一个行末带有冒号 `:` 的语句起始;同属于一个语句块中的语句，行首空白数量应该相等
+	注释：
+		#
+	操作符：
+		加+、减-、乘*、除/、商//、余%、幂**
+		x = x + 1可以写为X += 1
+		被除数÷除数=商··· ···余数
